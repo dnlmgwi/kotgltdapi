b0fecbcbaf0ab19078b6e454f1da5babc587003e
Daniel P. Mgawi <54575392+sketchdm@users.noreply.github.com>
2022-05-05 00:04:52 +0200

    Update lifecycles.js

--- src/api/team/content-types/team/lifecycles.js
+++ src/api/team/content-types/team/lifecycles.js
@@ -1,10 +1,13 @@
-const { customRandom, urlAlphabet } = require('nanoid')
+const { customAlphabet } = require('nanoid');
 
 module.exports = {
     beforeCreate(event) {
         //URL-friendly symbols
         //https://github.com/ai/nanoid#readme
-        const id = customRandom(urlAlphabet, 10)
+        const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        const id = customAlphabet(alphabet, 10);
+
+
         const { data, where, select, populate } = event.params;
 
         data.invite_code = id;
