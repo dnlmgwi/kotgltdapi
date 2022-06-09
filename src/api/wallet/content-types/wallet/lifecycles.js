cadfeb2387b7062a404d899d7a985ad7f61a15cb
Daniel P. Mgawi <pdmgawi@gmail.com>
2022-06-09 14:22:33 +0200

    Wallet-API
    - Planning needed

--- src/api/wallet/content-types/wallet/lifecycles.js
+++ src/api/wallet/content-types/wallet/lifecycles.js
@@ -1,16 +1,17 @@
 const { nanoid } = require("nanoid/non-secure");
 const { ApplicationError } = require("@strapi/utils").errors;
 
+
+
 module.exports = {
     beforeCreate(event) {
-        //URL-friendly symbols
-        //https://github.com/ai/nanoid#readme
         const id = nanoid(9);
         const { data, where, select, populate } = event.params;
-        data.invite_code = id;
 
         if (data.user === undefined) {
-            throw new ApplicationError('Please Assign Team Captain', 400);
+            throw new ApplicationError('Please Assign Wallet to User', 400);
         }
+
+        data.account_id = id;
     },
 };
