0f7885be7a0b6584b846702f2cced974288bef73
Daniel P. Mgawi <pdmgawi@gmail.com>
2022-06-25 01:17:22 +0200

    0.1.26
    - NanoID id (10 Uuid)
    - Event Ensures User and Event Is Selected

--- src/api/team/content-types/team/lifecycles.js
+++ src/api/team/content-types/team/lifecycles.js
@@ -1,13 +1,14 @@
-const { nanoid } = require("nanoid/non-secure");
 const { ApplicationError } = require("@strapi/utils").errors;
+const { customAlphabet } = require('nanoid');
+const alphabet = '123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz';
 
 module.exports = {
     beforeCreate(event) {
         //URL-friendly symbols
         //https://github.com/ai/nanoid#readme
-        const id = nanoid(9);
+        const id = customAlphabet(alphabet, 10);
         const { data, where, select, populate } = event.params;
-        data.invite_code = id;
+        data.invite_code = id();
 
         if (data.captain === undefined) {
             throw new ApplicationError('Please Assign Team Captain', 400);
